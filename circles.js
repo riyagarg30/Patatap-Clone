e578b7b24ec24df58108ae2e20d2a3d3808c41be
Riya Garg <riyagarg30@gmail.com>
2020-10-20 21:42:28 +0530

    Add files via upload

---
+++ circles.js
@@ -0,0 +1,16 @@
+// Create a Paper.js Path to draw a line into it:
+	//var path = new Path();
+	// Give the stroke a color
+	//path.strokeColor = 'red';
+	//var start = new Point(100, 100);
+	// Move to start and draw a line from there
+	//path.moveTo(start);
+	// Note the plus operator on Point objects.
+	// PaperScript does that for us, and much more!
+	//path.lineTo(start + [ 100, -50 ]);
+
+
+	
+
+	var myCircle = new Path.Circle(new Point(0, 0), 50);
+	myCircle.fillColor = 'purple';
